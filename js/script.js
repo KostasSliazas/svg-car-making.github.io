ccadd59b9e4fbe912ace22599e28ba254ac542cb
Kostas <kostas.sliazas@gmail.com>
2022-12-10 19:22:57 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function init () {
+  const elements = document.querySelectorAll('path')
+
+  function move (p) {
+    if (p) elements.forEach(e => { e.style.transform = `translate(${randomIntFromInterval(-2000, -5000)}px,${randomIntFromInterval(-2000, -5000)}px)` })
+    else {
+      const arr = Array.from(elements)
+      for (let i = 0; i < arr.length; i++) {
+        setTimeout(() => {
+          arr[i].style.removeProperty('transform')
+        }, i * 100)
+      }
+    }
+  }
+
+  function randomIntFromInterval (min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  move(1)
+
+  document.addEventListener('click', (e) => {
+    document.getElementsByClassName('hidden')[0].classList.remove('hidden')
+    setTimeout(() => move(), 1000)
+  }, { once: true })
+}
+document.addEventListener('DOMContentLoaded', init, { once: true })
